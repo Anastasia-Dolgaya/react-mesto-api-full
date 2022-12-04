57ee125397eedf73945b0d6800ee1d7a28372392
Anastasia Dolgaya <novial.da@novialtc.com>
2022-12-04 21:32:15 +0500

    fix: updated cors mw

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ const { cors } = require('./middlewares/cors');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(cors);
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -33,8 +34,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(requestLogger);
-
-app.use(cors);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
